e02957f342dd1671e7c374543a4c75366071775d
Wendy Leung <wendy.leung@holbertonschool.com>
2018-12-21 00:28:25 +0000

    add stack function

---
+++ ops4.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * _stack - Doesn't do anything
+ * @stack: stack
+ * @line_number: line number
+ */
+void _stack(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
